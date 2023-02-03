ef9f734703024b946ab1c5a7e4a1493d728d2ea5
myunghaklee <mh9716@naver.com>
2023-02-03 07:09:59 +0000

    fix error

--- gpu_empty_alarm.py
+++ gpu_empty_alarm.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 # +
 from email.mime.text import MIMEText
@@ -7,18 +8,16 @@ import smtplib
 import time
 import json
 
-    
 parser = argparse.ArgumentParser()
-parser.add_argument('--sleep_time', type=int, default=900, help = "GPU Usage Measurement Cycle")
-parser.add_argument('--gpu_id', default=[0], type=int, nargs='+', help = "Number of Used GPU")
-parser.add_argument('--it', type=int, default=5, help = "Number of measurements")
-
-parser.add_argument('--memory_usage', type=int, default=5, help = "The memory usage threshold for sending mail.")
-parser.add_argument('--memory_utill', type=int, default=5, help = "The memory utill threshold for sending mail.")
-
-parser.add_argument('--ID_s', type=str, required=True, help = "ID of the email to be sent")
-parser.add_argument('--PW_s', type=str, required=True, help = "PW of the email to be sent")
-parser.add_argument('--ID_r', type=str, required=True, help = "ID of the email to be sent")
+parser.add_argument('--sleep_time', type=int, default=900, help="GPU Usage Measurement Cycle")
+parser.add_argument('--gpu_id', default=[0], type=int, nargs='+', help="Number of Used GPU")
+parser.add_argument('--it', type=int, default=5, help="Number of measurements")
+parser.add_argument('--memory_usage', type=int, default=50, help="The memory usage threshold for sending mail.")
+parser.add_argument('--memory_utill', type=int, default=5000, help="The memory utill threshold for sending mail.")
+parser.add_argument('--ID_s', type=str, required=True, help="ID of the email to be sent")
+parser.add_argument('--PW_s', type=str, required=True, help="PW of the email to be sent")
+parser.add_argument('--ID_r', type=str, required=True, help="ID of the email to be received")
+parser.add_argument('--mail_check', action='store_true')
 
 gpu_num = len(args.gpu_id)
 
@@ -34,7 +33,8 @@ DEFAULT_ATTRIBUTES = (
     'name'
 )
 
-def get_process()
+
+def get_process():
     process = subprocess.check_output("ps -auxww $$",shell=True,executable="/bin/bash")
     process = str(process.decode())
     process_list = []
@@ -45,7 +45,8 @@ def get_process()
                 continue
             process_list.append(command)
     return process_list
-    
+
+
 def get_gpu_info(nvidia_smi_path='nvidia-smi', keys=DEFAULT_ATTRIBUTES, no_units=True):
     nu_opt = '' if not no_units else ',nounits'
     cmd = '%s --query-gpu=%s --format=csv,noheader%s' % (nvidia_smi_path, ','.join(keys), nu_opt)
@@ -56,17 +57,48 @@ def get_gpu_info(nvidia_smi_path='nvidia-smi', keys=DEFAULT_ATTRIBUTES, no_units
     info = sorted(info, key=lambda a:int(a['index']))
     return info
 
-def main(args):
 
-    info = get_gpu_info()
-    info = [A[i] for i in args.gpu_id]
-    gpu_name = info[0]['name']
+def get_contents(info):
+    content = f'GPU name : {info[0]["name"]}\n'
+    content = f'GPU id : {args.gpu_id}\n'
+    content += f'GPU memory_used : {[int(d["memory.used"]) for d in info]}\n'
+    content += f'GPU utilization : {[int(d["utilization.gpu"]) for d in info]}\n'
+    content += f'GPU temperature : {[int(d["temperature.gpu"]) for d in info]}\n'
+    content += '\n' * 3
+    content += 'process\n'
+    process_list = get_process()
+    for p in process_list:
+        content += f"{p}\n"
+
+    return content
+
+
+def main(args):
+    if args.mail_check:
+        info = get_gpu_info()
+        info = [info[i] for i in args.gpu_id]
+        gpu_name = info[0]['name']
+
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.starttls()
+        print("login")
+        s.login(args.ID_s, args.PW_s)
+
+        title = "Mail Check."
+        content = "get_contents(info)"
+
+        msg = MIMEText(content)
+        msg['Subject'] = title
+
+        print("Mail check!!!")
+        print("The email was sent to check the email ID and password were appropriate.")
+        s.sendmail(args.ID_s, args.ID_r, msg.as_string())
 
     while(True):
         try:
             print("GPU is Not Empty")
             memory_used, utilization, temperature = 0, 0, 0
-            
+
             for _ in range(args.it):
                 info = get_gpu_info()
                 info = [info[i] for i in args.gpu_id]  # get gpu info that you selected
@@ -92,24 +124,17 @@ def main(args):
         except KeyboardInterrupt:
             break
 
+    info = get_gpu_info()
+    info = [info[i] for i in args.gpu_id]
+    gpu_name = info[0]['name']
+
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-
     print("login")
     s.login(args.ID_s, args.PW_s)
 
     title = f"The GPU device is empty. {gpu_name}"
-    content = f'GPU name : {gpu_name}\n'
-    content = f'GPU id : {args.gpu_id}\n'
-    content += f'GPU memory_used : {[int(d["memory.used"]) for d in A]}\n'
-    content += f'GPU utilization : {[int(d["utilization.gpu"]) for d in A]}\n'
-    content += f'GPU temperature : {[int(d["temperature.gpu"]) for d in A]}\n'
-    content += f'\n' * 3
-    content += f'process\n'
-
-    process_list = get_process()
-    for p in process_list:
-        content += f"{p}\n"
+    content = get_contents(info)
 
     msg = MIMEText(content)
     msg['Subject'] = title
@@ -118,7 +143,8 @@ def main(args):
     print("title : ", title)
     print("content : ", content)
     s.sendmail(args.ID_s, args.ID_r, msg.as_string())
-    
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
     main(args)
